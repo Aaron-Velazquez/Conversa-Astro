6ba77c97c58e507d981315195f90e1725ce8d03c
Aaron Velazquez <aaronv152@gmail.com>
2024-05-13 14:02:08 -0400

    deploy test

--- .netlify/functions-internal/ssr/chunks/planes_eyJcnxIR.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/planes_CHLJQ6lj.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/soporte_B3OjnuFK.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/soporte_C2fk-anc.mjs');
-
-export { page };
